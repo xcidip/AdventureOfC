f6cf4e96425591af833ad3c7c70d4db69b2998d7
xcidip <xcidip@tutanota.com>
2024-06-04 08:30:52 +0200

    v0.0.3 added basic inventory and stuff

--- main.c
+++ main.c
@@ -3,14 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Player {
-  char name[50];
-  int HP;
-  int MP;
-  char inv[10][10];
-};
+#define INV_SLOTS 30
+#define GAME_VERSION 0.0.2
 
 struct Item {
+  int id;
   char name[30];
   char itemType[20];
 };
@@ -20,17 +17,56 @@ struct Potion {
   int healValue;
 };
 
+struct Player {
+  char name[50];
+  int HP;
+  int MP;
+  int inv[INV_SLOTS];
+};
+
+struct Spell {
+  char name[30];
+  int dmg;
+  char type[10]; // shadow, fire, water
+};
+
+void healPlayer(struct Player plr, int amount) { plr.HP += amount; }
+
+void addToInventory(int id, int *inv) {
+  for (int i = 0; i < INV_SLOTS; i++) {
+    if (inv[i] < 0) {
+      inv[i] = id;
+      printf("%d was added to inventory", id);
+      return;
+    }
+    if (i == INV_SLOTS - 1) {
+      printf("All %d inventory slots are full", INV_SLOTS);
+    }
+  }
+}
+
 void PrintStats(struct Player plr) {
   printf("Name: %s\nHP:%d\nMP:%d", plr.name, plr.HP, plr.MP);
 }
 
+void LookupItemName(int id, ) {}
+
+void printInventory(int *inv) {
+  printf("-------------------");
+  printf("const char *restrict, ...");
+}
+
 int main() {
   printf("Welcome to the Adventure Of C the small cube\n");
-  printf("This is version 0.0.2\n");
+  struct Player plr = {"Player Playerson", 100, 100};
 
-  struct Player player1 = {"Player Playerson", 100, 100};
+  struct Item ItemDictionary[200] = {
 
-  struct Potion healPotion = {"Healing potion 15HP", "Healing Potion", 15};
+  };
 
-  PrintStats(player1);
+  struct Potion healPotion = {1, "Healing potion 15HP", "Healing Potion", 15};
+
+  addToInventory(healPotion.item.id, plr.inv);
+
+  PrintStats(plr);
 }
